fefcfeb23a4b36d216a2eb8786671370738cc614
brunoJorquera <jorquera.b18@gmail.com>
2021-09-28 12:55:53 -0700

    rollbar added

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const express = require("express")
 const path = require('path')
 const app = express()
 
+const Rollbar = require("rollbar")
+const rollbar = new Rollbar({
+    accessToken: '83645117e0c94fca917c364866c89286',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
+
     app.get('/', (req,res) =>{
         res.sendFile(path.join(__dirname, './client/index.html'))
+        rollbar.info("Html was monitored successfuly!")
 })
 
 const port = process.env.PORT || 5656
+
+app.use(rollbar.errorHandler())
 
 app.listen(port, () => console.log(`ZooWeeMama! Server ${port} is running!`))
